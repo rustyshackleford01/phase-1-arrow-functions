6f670ce28f61f06814bc194a820233fa6d844a7e
Samantha Webb <samantha.webb2436@gmail.com>
2025-02-15 05:34:00 -0600

    Complete

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(a, b) {
+     return a / b;
+    };
+
+    // Arrow function for squaring a number
+const square = x => x * x;
+
+// Arrow function for adding two numbers
+const add = (a, b) => a + b;
